abcab9dc9c5cb76cf2a1856ebf74079e126ceed4
moyuhaokane <1649189468@qq.com>
2021-11-08 22:03:15 +0800

    实现 readonly 功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,21 +1,14 @@
 /** @format */
 
-import {track, trigger} from './effect'
+import {mutableHandlers, readonlyHandlers} from './baseHandler'
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
+  return createReactiveObject(raw, mutableHandlers)
+}
+export function readonly(raw) {
+  return createReactiveObject(raw, readonlyHandlers)
+}
 
-      track(target, key)
-      return res
-    },
-
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-
-      trigger(target, key)
-      return res
-    },
-  })
+function createReactiveObject(target, baseHandles) {
+  return new Proxy(target, baseHandles)
 }
