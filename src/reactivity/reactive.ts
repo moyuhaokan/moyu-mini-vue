614ce141a127033bc6be4fc1ed29f501a578e0de
moyuhaokane <1649189468@qq.com>
2021-11-09 22:54:23 +0800

    实现 isReadonly 和 isReactive 功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -2,11 +2,23 @@
 
 import {mutableHandlers, readonlyHandlers} from './baseHandler'
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive',
+  IS_READONLY = '__v_isReadonly',
+}
 export function reactive(raw) {
   return createReactiveObject(raw, mutableHandlers)
 }
 export function readonly(raw) {
   return createReactiveObject(raw, readonlyHandlers)
+}
+
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY]
 }
 
 function createReactiveObject(target, baseHandles) {
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,12 +1,14 @@
 /** @format */
 
-import {readonly} from '../reactive'
+import {isReadonly, readonly} from '../reactive'
 
 describe('readonly', () => {
   it('should make nested values readonly', () => {
     const original = {foo: 1, bar: {baz: 2}}
     const wrapped = readonly(original)
     expect(wrapped).not.toBe(original)
+    expect(isReadonly(wrapped)).toBe(true)
+    expect(isReadonly(original)).toBe(false)
     expect(wrapped.foo).toBe(1)
   })
 
