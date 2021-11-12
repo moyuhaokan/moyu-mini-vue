3556b4c9c6a6ea8d2a590c90f58c73d02de46c7a
moyuhaokane <1649189468@qq.com>
2021-11-12 23:40:11 +0800

    优化 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,41 +1,56 @@
-import { extend } from "../shared";
+/** @format */
 
+import {extend} from '../shared'
+let activeEffect
+let shouldTrack = false
 class ReactiveEffect {
   private _fn: any
-  deps = [];
-  active = true;
-  onStop?: () => void;
+  deps = []
+  active = true
+  onStop?: () => void
   public scheduler: Function | undefined
   constructor(fn, scheduler?: Function) {
     this._fn = fn
-    this.scheduler = scheduler;
+    this.scheduler = scheduler
   }
   run() {
+    if (!this.active) {
+      return this._fn()
+    }
+
+    // 应该收集
+    shouldTrack = true
     activeEffect = this
-    return this._fn();
+    const r = this._fn()
+
+    // 重置
+    shouldTrack = false
+
+    return r
   }
 
   stop() {
     if (this.active) {
-      cleanupEffect(this);
+      cleanupEffect(this)
       if (this.onStop) {
-        this.onStop();
+        this.onStop()
       }
       this.active = false
     }
   }
-
 }
-
 
 function cleanupEffect(effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect)
   })
+  // 把 effect.deps 清空
+  effect.deps.length = 0
 }
 
 const targetMap = new Map()
 export function track(target, key) {
+  if (!isTracking()) return
   // target -> key -> dep
   let depsMap = targetMap.get(target)
   if (!depsMap) {
@@ -48,20 +63,24 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
-  if (!activeEffect) return;
+
+  // 看看 dep 之前有没有添加过，添加过的话 那么就不添加了
+  if (dep.has(activeEffect)) return
   dep.add(activeEffect)
   activeEffect.deps.push(dep)
 }
-
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined
+}
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
 
   for (const effect of dep) {
     if (effect.scheduler) {
-      effect.scheduler();
+      effect.scheduler()
     } else {
-      effect.run();
+      effect.run()
     }
   }
 }
@@ -71,14 +90,13 @@ export function effect(fn, options: any = {}) {
   // fn
   const _effect = new ReactiveEffect(fn, options.scheduler)
 
-  extend(_effect, options);
+  extend(_effect, options)
   _effect.run()
 
-  const runner: any = _effect.run.bind(_effect);
-  runner.effect = _effect;
-  return runner;
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
 }
-
 
 export function stop(runner) {
   runner.effect.stop()
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,7 +1,7 @@
 /** @format */
 
-import { effect, stop } from '../effect'
-import { reactive } from '../reactive'
+import {effect, stop} from '../effect'
+import {reactive} from '../reactive'
 
 describe('effect', () => {
   it('happy path', () => {
@@ -21,84 +21,83 @@ describe('effect', () => {
     expect(nextAge).toBe(12)
   })
 
-  it("should return runner when call effect", () => {
+  it('should return runner when call effect', () => {
     // 当调用 runner 的时候可以重新执行 effect.run
     // runner 的返回值就是用户给的 fn 的返回值
-    let foo = 0;
+    let foo = 0
     const runner = effect(() => {
-      foo++;
-      return foo;
-    });
-
-    expect(foo).toBe(1);
-    runner();
-    expect(foo).toBe(2);
-    expect(runner()).toBe(3);
-  });
-
-  it("scheduler", () => {
-    let dummy;
-
-    let run: any;
-
-    const scheduler = jest.fn(() => {
-      run = runner;
+      foo++
+      return foo
     })
-    const obj = reactive({ foo: 1 });
-    const runner = effect(
-      () => {
-        dummy = obj.foo;
 
-      },
-      { scheduler }
-    )
-
-    expect(scheduler).not.toHaveBeenCalled();
-    expect(dummy).toBe(1);
-
-    obj.foo++;
-    expect(scheduler).toHaveBeenCalledTimes(1);
-
-    expect(dummy).toBe(1);
-    run();
-    expect(dummy).toBe(2);
+    expect(foo).toBe(1)
+    runner()
+    expect(foo).toBe(2)
+    expect(runner()).toBe(3)
   })
 
-  it("stop", () => {
-    let dummy;
-    const obj = reactive({ prop: 1 });
+  it('scheduler', () => {
+    let dummy
+
+    let run: any
+
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({foo: 1})
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      {scheduler}
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+
+    expect(dummy).toBe(1)
+    run()
+    expect(dummy).toBe(2)
+  })
+
+  it('stop', () => {
+    let dummy
+    const obj = reactive({prop: 1})
     const runner = effect(() => {
-      dummy = obj.prop;
+      dummy = obj.prop
     })
 
-    obj.prop = 2;
-    expect(dummy).toBe(2);
-    stop(runner);
-    obj.prop = 3;
-    expect(dummy).toBe(2);
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    // obj.prop = 3;
+    obj.prop++
+    expect(dummy).toBe(2)
 
-
-    runner();
+    runner()
     expect(dummy).toBe(3)
   })
 
-  it("onStop", () => {
+  it('onStop', () => {
     const obj = reactive({
-      foo: 1
+      foo: 1,
     })
 
-    const onStop = jest.fn();
-    let dummy;
+    const onStop = jest.fn()
+    let dummy
     const runner = effect(
       () => {
-        dummy = obj.foo;
+        dummy = obj.foo
       },
       {
         onStop,
       }
-    );
+    )
 
-    stop(runner);
-    expect(onStop).toBeCalledTimes(1);
+    stop(runner)
+    expect(onStop).toBeCalledTimes(1)
   })
 })
