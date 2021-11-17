45e4cb83e6cfb55071eb4af3a9503434cf3388c5
moyu <1649189468@qq.com>
2021-11-17 19:36:38 +0800

    实现 ref 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import {extend} from '../shared'
+import { extend } from '../shared'
 let activeEffect
 let shouldTrack = false
 class ReactiveEffect {
@@ -64,18 +64,26 @@ export function track(target, key) {
     depsMap.set(key, dep)
   }
 
-  // 看看 dep 之前有没有添加过，添加过的话 那么就不添加了
-  if (dep.has(activeEffect)) return
-  dep.add(activeEffect)
-  activeEffect.deps.push(dep)
+  trackEffects(dep)
 }
-function isTracking() {
+
+export function trackEffects(dep) {
+  // 看看 dep 之前有没有添加过，添加过的话 那么就不添加了
+  if (dep.has(activeEffect)) return;
+
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+export function isTracking() {
   return shouldTrack && activeEffect !== undefined
 }
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
+  triggerEffects(dep)
+}
 
+export function triggerEffects(dep) {
   for (const effect of dep) {
     if (effect.scheduler) {
       effect.scheduler()
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,3 +5,6 @@ export const extend = Object.assign
 export const isObject = (value) => {
   return value !== null && typeof value === 'object'
 }
+export const hasChanged = (val, newValue) => {
+  return !Object.is(val, newValue);
+};
